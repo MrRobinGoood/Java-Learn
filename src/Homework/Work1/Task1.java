c93ea7dbf75613f174247dd6d7e576586b732d46
MrRobinGoood <MRobinGoood@yandex.ru>
2021-10-12 15:13:08 +0400

    Task1 reformat code. Create task2.

--- src/Homework/Work1/Task1.java
+++ src/Homework/Work1/Task1.java
@@ -3,7 +3,7 @@ package Homework.Work1;
 import java.util.Scanner;
 
 public class Task1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Task1 task = new Task1();
 
         Scanner input = new Scanner(System.in);
@@ -14,17 +14,16 @@ public class Task1 {
     }
 
     public String isLucky(int number) {
-       int partNumber1 , partNumber2 , sumPart1, sumPart2;
-       partNumber1 = number/1000;
-       partNumber2 = number%1000;
-       sumPart1 = (partNumber1/100)+((partNumber1/10)%10)+(partNumber1%10);
-       sumPart2 = (partNumber2/100)+((partNumber2/10)%10)+(partNumber2%10);
-        if(sumPart1 == sumPart2){
-           return "Lucky number";
-       }
-       else{
-           return "Not lucky number";
-       }
+        int partNumber1, partNumber2, sumPart1, sumPart2;
+        partNumber1 = number / 1000;
+        partNumber2 = number % 1000;
+        sumPart1 = (partNumber1 / 100) + ((partNumber1 / 10) % 10) + (partNumber1 % 10);
+        sumPart2 = (partNumber2 / 100) + ((partNumber2 / 10) % 10) + (partNumber2 % 10);
+        if (sumPart1 == sumPart2) {
+            return "Lucky number";
+        } else {
+            return "Not lucky number";
+        }
     }
 
-    }
+}
---
+++ src/Homework/Work1/Task2.java
@@ -0,0 +1,7 @@
+package Homework.Work1;
+
+public class Task2 {
+    public static void main(String[] args){
+
+    }
+}
