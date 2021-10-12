bd3ec62d437d5a8858565911a363acef41c628f4
MrRobinGoood <MRobinGoood@yandex.ru>
2021-10-12 15:25:43 +0400

    Rebuild isLucky in Task1 for boolean.

--- src/Homework/Work1/Task1.java
+++ src/Homework/Work1/Task1.java
@@ -13,17 +13,14 @@ public class Task1 {
         System.out.println(task.isLucky(inputValue));
     }
 
-    public String isLucky(int number) {
+    public boolean isLucky(int number) {
         int partNumber1, partNumber2, sumPart1, sumPart2;
         partNumber1 = number / 1000;
         partNumber2 = number % 1000;
         sumPart1 = (partNumber1 / 100) + ((partNumber1 / 10) % 10) + (partNumber1 % 10);
         sumPart2 = (partNumber2 / 100) + ((partNumber2 / 10) % 10) + (partNumber2 % 10);
-        if (sumPart1 == sumPart2) {
-            return "Lucky number";
-        } else {
-            return "Not lucky number";
-        }
-    }
+        return (sumPart1 == sumPart2);
 
+
+    }
 }
--- src/Homework/Work1/Task2.java
+++ src/Homework/Work1/Task2.java
@@ -1,7 +1,14 @@
 package Homework.Work1;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args){
+        Task1 task = new Task1();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int inputValue = input.nextInt();
 
+        System.out.println(task.isLucky(inputValue));
     }
 }
