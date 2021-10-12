b6751f5c1846c0d12d287f77d20b0fa91e1c710c
MrRobinGoood <MRobinGoood@yandex.ru>
2021-10-12 13:13:39 +0400

    Test branch

--- src/Homework/Work1/Task1.java
+++ src/Homework/Work1/Task1.java
@@ -2,6 +2,6 @@ package Homework.Work1;
 
 public class Task1 {
     public static void main(String[] args){
-        System.out.println("Test");
+        System.out.println("Test branch");
     }
 }
