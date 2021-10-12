1784c248a1de3e20739bdccf239effc2104b55cf
MrRobinGoood <MRobinGoood@yandex.ru>
2021-10-12 16:35:21 +0400

    Fixed Task2.

--- src/Homework/Work1/Task2.java
+++ src/Homework/Work1/Task2.java
@@ -5,21 +5,15 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Task1 task = new Task1();
-        Task2 task2 = new Task2();
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input number: ");
-        int inputValue = input.nextInt();
 
-        System.out.println(task.isLucky(inputValue));
+        int sumLuckyNumbers = 0;
+        for (int i = 100000; i < 1000000; i++) {
+            if (task.isLucky(i)) {
+                sumLuckyNumbers += i;
+            }
 
-        if (task.isLucky(inputValue)) {
-            System.out.println(task2.numberSum(inputValue));
         }
-    }
-
-    public int numberSum(int number) {
-        return (((number / 100) % 10) + ((number / 10) % 10) + (number % 10)) * 2;
-
+        System.out.println(sumLuckyNumbers);
     }
 }
