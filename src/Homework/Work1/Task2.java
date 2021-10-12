ee1af1816a8f61478399657e84a7c5b147dd1a6e
MrRobinGoood <MRobinGoood@yandex.ru>
2021-10-12 17:21:44 +0400

    Create method diapason for Task2 and Task4.

--- src/Homework/Work1/Task2.java
+++ src/Homework/Work1/Task2.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Task2 task2 = new Task2();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input diapason start: ");
+        int inputStart = input.nextInt();
+        System.out.print("Input diapason end: ");
+        int inputEnd = input.nextInt();
+
+        System.out.println(task2.sumLuckyNumInDiapason(inputStart, inputEnd));
+
+    }
+
+    public int sumLuckyNumInDiapason(int start, int end) {
         Task1 task = new Task1();
-
-
         int sumLuckyNumbers = 0;
-        for (int i = 100000; i < 1000000; i++) {
+        for (int i = start; i <= end; i++) {
             if (task.isLucky(i)) {
                 sumLuckyNumbers += i;
             }
-
         }
-        System.out.println(sumLuckyNumbers);
+        return sumLuckyNumbers;
+
     }
+
 }
--- src/Homework/Work1/Task4.java
+++ src/Homework/Work1/Task4.java
@@ -4,20 +4,28 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
+        Task4 task4 = new Task4();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input diapason start: ");
+        int inputStart = input.nextInt();
+        System.out.print("Input diapason end: ");
+        int inputEnd = input.nextInt();
 
+        System.out.println(task4.sumPrimeNumInDiapason(inputStart, inputEnd));
+
+    }
+
+    public int sumPrimeNumInDiapason(int start, int end) {
         Task3 task3 = new Task3();
         int sumPrimeNumbers = 0;
-        for (int i = 1000; i <= 2000; i++) {
+        for (int i = start;i <= end; i++) {
             if (task3.isPrime(i)) {
                 sumPrimeNumbers += i;
             }
 
         }
-        System.out.println(sumPrimeNumbers);
-
-
+        return sumPrimeNumbers;
     }
-
 }
 
 
