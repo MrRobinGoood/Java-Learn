f8db6106b9da0fc12978b570066da1decb23e89c
MrRobinGoood <MRobinGoood@yandex.ru>
2021-11-06 14:25:27 +0400

    Task4 complete. I'm not sure, what task4 is true.

--- src/Homework/Work2/Task4.java
+++ src/Homework/Work2/Task4.java
@@ -1,4 +1,24 @@
 package Homework.Work2;
 
 public class Task4 {
+    public static void main(String[] args) {
+        Task4 task4 = new Task4();
+        Task3 task3 = new Task3();
+        Task1 task1 = new Task1();
+        int[] array = task1.createArray(5);
+        task1.fillArrayByRandom(array, 0, 100);
+        task1.printArray(array);
+
+        System.out.println();
+
+        int maxInd, minInd, maxNum;
+        maxInd = task3.maxNumIndex(array);
+        minInd = task3.minNumIndex(array);
+        maxNum = array[maxInd];
+        array[maxInd] = array[minInd];
+        array[minInd] = maxNum;
+        task1.printArray(array);
+    }
+
+
 }
