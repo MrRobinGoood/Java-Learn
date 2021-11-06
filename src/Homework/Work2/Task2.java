208d5e93a2fd95bb3eaf7d2badc1a88cc5edbe33
MrRobinGoood <MRobinGoood@yandex.ru>
2021-11-06 12:09:18 +0400

    Create method sumPositiveNumbers.

---
+++ src/Homework/Work2/Task2.java
@@ -0,0 +1,24 @@
+package Homework.Work2;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        int[] array = task1.createArray(5);
+        array = task1.fillArrayByRandom(array);
+        task1.printArray(array);
+        System.out.println(task2.sumPositiveNumbers(array));
+    }
+
+    public int sumPositiveNumbers(int[] number) {
+        int sumPosNum = 0;
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] > 0) {
+                sumPosNum += number[i];
+            }
+        }
+        return sumPosNum;
+    }
+
+}
