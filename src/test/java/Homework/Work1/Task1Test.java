c7e46c861e7413e06f4bcdb94b400bc1a8b98a6d
MrRobinGoood <MRobinGoood@yandex.ru>
2022-05-16 12:10:51 +0400

    Complete Task1Test.

--- src/test/java/Homework/Work1/Task1Test.java
+++ src/test/java/Homework/Work1/Task1Test.java
@@ -1,16 +1,14 @@
 package Homework.Work1;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class Task1Test {
 
     @Test
-    void main() {
-    }
-
-    @Test
     void isLucky() {
+        Task1 task1 = new Task1();
+        boolean actual = task1.isLucky(123321);
+        assertTrue(actual);
     }
 }
